f0cde1d5d992e1868200d8179222c1a88a907b1f
Akeri <samaescii@gmail.com>
2025-05-29 23:07:50 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const numeroSecreto = Math.floor(Math.random() * 100) + 1;
+const inputNumero = document.getElementById('numero');
+const botonAdivinar = document.getElementById('adivinar');
+const mensaje = document.getElementById('mensaje');
 
-setupCounter(document.querySelector('#counter'))
+botonAdivinar.addEventListener('click', () => {
+    const numeroJugador = parseInt(inputNumero.value);
+
+    if (isNaN(numeroJugador) || numeroJugador < 1 || numeroJugador > 100) {
+        mensaje.textContent = 'Por favor, ingresa un número válido entre 1 y 100.';
+    } else if (numeroJugador === numeroSecreto) {
+        mensaje.innerHTML = '¡Felicidades! ¡Adivinaste el número! <br><img src="https://i.makeagif.com/media/6-04-2016/-EmzU5.gif"/>';
+
+} else if (numeroJugador < numeroSecreto) {
+    mensaje.innerHTML = 'El número es más alto <br><img class="respuesta-img" src="https://i.gifer.com/origin/ff/ff28442677f10a696b93a6ac90ace3ea_w200.gif" />';
+} else {
+    mensaje.innerHTML = 'El número es más bajo <br><img class="respuesta-img" src="https://i.pinimg.com/originals/82/ed/30/82ed30521136fb22d77c7e554621d67c.gif" />';
+}
+
+});
